5b0c40321dc3cad978ad9b3942bed28b6249e059
hmasad09 <hmasad09@gmail.com>
2018-08-23 23:55:00 +0800

    Seeder created for Product and Review Table

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,5 +5,11 @@ use Faker\Generator as Faker;
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
         //
+        'name'=> $faker ->name,
+        'price'=> $faker ->numberBetween(300,3000),
+        'origin'=> $faker ->country,
+        'discount'=> $faker ->numberBetween(5,25),
+        'stock'=> $faker ->randomDigit,
+
     ];
 });
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -1,9 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Model\Product;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id'=> Product::all()->random(),
+        'customerName' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(1,5)
     ];
 });

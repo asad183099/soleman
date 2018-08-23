0e7e1491c9de179a2e8cb572e6710af48f04a0d0
hmasad09 <hmasad09@gmail.com>
2018-08-23 23:05:44 +0800

    Product and review table migration done

--- database/migrations/2018_08_23_135932_create_products_table.php
+++ database/migrations/2018_08_23_135932_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name',100)->unique();
+            $table->double('price');
+            $table->string('origin');
+            $table->double('discount');
+            $table->integer('stock');
             $table->timestamps();
         });
     }
